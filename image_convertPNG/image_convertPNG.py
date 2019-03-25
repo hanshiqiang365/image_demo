3472ddbf5a54f95998209efb8c048714b5eb1109
hanshiqiang365 <shiqiang.han@accenture.com>
2019-03-25 18:31:34 +0800

    Add files via upload

---
+++ image_convertPNG/image_convertPNG.py
@@ -0,0 +1,19 @@
+﻿import PIL.Image as Image
+
+def transparent_back(img):
+    img = img.convert('RGBA')
+    L, H = img.size
+    color_0 = (255,255,255,255)
+    for h in range(H):
+        for l in range(L):
+            dot = (l,h)
+            color_1 = img.getpixel(dot)
+            if color_1 == color_0:
+                color_1 = color_1[:-1] + (0,)
+                img.putpixel(dot,color_1)
+    return img
+
+if __name__ == '__main__':
+    img=Image.open('testimage.jpg')
+    img=transparent_back(img)
+    img.save('testimage_output.png')
